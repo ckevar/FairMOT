d561a11f3b4f40f9eaa2647648ad2cdf215a9be5
ckevar <ckevar@gmail.com>
2025-09-09 10:23:10 +0200

    modified:   experiments/kitti_multiclass_dla34.sh

--- experiments/kitti_multiclass_dla34.sh
+++ experiments/kitti_multiclass_dla34.sh
@@ -5,5 +5,7 @@ python train.py mot \
   --data_cfg "../src/lib/cfg/kitti_multiclass.json" \
   --gpus 1 \
   --reference_model "../../../../detectors/fairmot/dla34-ba72cf86.pth" \
+  --num_classes 9 \
+  --print_iter 20 \
   --batch_size 2
 cd ..
